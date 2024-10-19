ca56e4c0d78b3a8c3ff161e36c17a79fc1f6a543
TTC-CCF <ipauldj.cs10@nycu.edu.tw>
2024-10-19 09:25:40 +0800

    feat: add ai code review

--- .github/scripts/api.py
+++ .github/scripts/api.py
@@ -4,80 +4,104 @@ import subprocess
 import sys
 import json
 import requests
-
-commit_users = {}
-user_comments = {}
-commit_count = {}
+from gen_review import get_review_summary
 
 
-def get_commits(token, repo, pr_num):
-    global commit_users, user_comments, commit_count
+def get_diff_and_reviewers(token, repo, pr_num):
+    cmd = f"""
+    curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer {token}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/{repo}/pulls/{pr_num}
+    """
+    res = subprocess.run(cmd, capture_output=True, shell=True)
+    json_res = json.loads(res.stdout.decode())
+
+    head_sha = json_res["head"]["sha"]
+    base_sha = json_res["base"]["sha"]
+
+    diff = extract_diff(head_sha, base_sha)
+
+    reviewers = [r["login"] for r in json_res["requested_reviewers"]]
+
+    return diff, reviewers
+
+
+def get_contributors(token, repo, pr_num):
+    commit_count, commit_stat = {}, {}
 
     cmd = f"""
-curl -L \
--H "Accept: application/vnd.github+json" \
--H "Authorization: Bearer {token}" \
--H "X-GitHub-Api-Version: 2022-11-28" \
-https://api.github.com/repos/{repo}/pulls/{pr_num}/commits
-"""
+    curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer {token}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/{repo}/pulls/{pr_num}/commits
+    """
     res = subprocess.run(cmd, capture_output=True, shell=True)
     json_res = json.loads(res.stdout.decode())
 
     for commit in json_res:
-        github_id = commit["author"]["login"]
-        sha = commit["sha"]
-        commit_users[sha] = github_id
-        user_comments[github_id] = []
-        commit_count[github_id] = commit_count.get(github_id, 0) + 1
-
-    return commit_users
-
-
-def get_review_comments(token, repo, pr_num):
-    global user_comments
-    cmd = f"""
-curl -L \
--H "Accept: application/vnd.github+json" \
--H "Authorization: Bearer {token}" \
--H "X-GitHub-Api-Version: 2022-11-28" \
-https://api.github.com/repos/{repo}/pulls/{pr_num}/comments
-"""
-    res = subprocess.run(cmd, capture_output=True, shell=True)
-    json_res = json.loads(res.stdout.decode())
-
-    for comment in json_res:
-        if comment["user"]["login"] in commit_users.values():
-            continue
-
-        original_commit_id = comment["original_commit_id"]
-
-        if original_commit_id in commit_users:
-            user = commit_users[original_commit_id]
-            user_comments[user].append(
-                {"reviewer_id": comment["user"]["login"], "comment": comment["body"]}
+        url = commit["url"]
+        cmd = f"""
+        curl -L \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer {token}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        {url}
+        """
+        res = subprocess.run(cmd, capture_output=True, shell=True)
+        _json_res = json.loads(res.stdout.decode())
+        try:
+            author = _json_res["author"]["login"]
+            commit_count[author] = commit_count.get(author, 0) + 1
+            author_stat = commit_stat.get(
+                author, {"additions": 0, "deletions": 0, "total": 0}
             )
 
+            for key in author_stat.keys():
+                author_stat[key] += _json_res["stats"][key]
 
-def post_request(repo, endpoint):
-    contributors = []
-    
-    for github_id in commit_count.keys():
-        contributor = {
-            "github_id": github_id,
-            "commit_count": commit_count[github_id],
-            "review_comments": user_comments[github_id],
+            commit_stat[author] = author_stat
+        except:
+            print(json.dumps(_json_res, indent=4))
+
+    return [
+        {
+            "github_id": author,
+            "commit_count": commit_count[author],
+            "stats": commit_stat[author],
         }
+        for author in commit_stat.keys()
+    ]
 
-        contributors.append(contributor)
 
-    body = {"repository": repo, "contributors": contributors}
+def extract_diff(head, base):
+    cmd = f"git diff {base}..{head}"
+    res = subprocess.run(cmd, capture_output=True, shell=True)
+    return res.stdout.decode()
+
+
+def post_request(repo, endpoint, contributors, reviewers, summary):
+
+    body = {
+        "repository": repo,
+        "contributors": contributors,
+        "summary": summary,
+        "reviewers": reviewers,
+    }
     print(json.dumps(body, indent=4))
     requests.post(f"{endpoint}/api/v1/pr", json=body)
 
 
-if __name__ == "__main__":
-    token, repo, pr_num, endpoint = sys.argv[1:5]
+def main():
+    token, repo, pr_num, endpoint, genmini_api_key = sys.argv[1:6]
 
-    get_commits(token, repo, pr_num)
-    get_review_comments(token, repo, pr_num)
-    post_request(repo, endpoint)
+    diff, reviewers = get_diff_and_reviewers(token, repo, pr_num)
+    summary = get_review_summary(diff, genmini_api_key)
+    contributors = get_contributors(token, repo, pr_num)
+    post_request(repo, endpoint, contributors, reviewers, summary)
+
+
+if __name__ == "__main__":
+    main()
---
+++ .github/scripts/gen_review.py
@@ -0,0 +1,185 @@
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import json
+import os
+from typing import List
+
+import google.generativeai as genai
+import requests
+from loguru import logger
+
+
+
+
+def get_review_prompt(extra_prompt: str = "") -> str:
+    """Get a prompt template"""
+    template = f"""
+    This is a pull request or part of a pull request if the pull request is very large.
+    Suppose you review this PR as an excellent software engineer and an excellent security engineer.
+    Can you tell me the issues with differences in a pull request and provide suggestions to improve it?
+    You can provide a review summary and issue comments per file if any major issues are found.
+    Always include the name of the file that is citing the improvement or problem.
+    In the next messages I will be sending you the difference between the GitHub file codes, okay?
+    """
+    return template
+
+
+def get_summarize_prompt() -> str:
+    """Get a prompt template"""
+    template = """
+    Can you summarize this for me?
+    It would be good to stick to highlighting pressing issues and providing code suggestions to improve the pull request.
+    Here's what you need to summarize:
+    """
+    return template
+
+
+def create_a_comment_to_pull_request(
+    github_token: str,
+    github_repository: str,
+    pull_request_number: int,
+    git_commit_hash: str,
+    body: str,
+):
+    """Create a comment to a pull request"""
+    headers = {
+        "Accept": "application/vnd.github.v3.patch",
+        "authorization": f"Bearer {github_token}",
+    }
+    data = {"body": body, "commit_id": git_commit_hash, "event": "COMMENT"}
+    url = f"https://api.github.com/repos/{github_repository}/pulls/{pull_request_number}/reviews"
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response
+
+
+def chunk_string(input_string: str, chunk_size) -> List[str]:
+    """Chunk a string"""
+    chunked_inputs = []
+    for i in range(0, len(input_string), chunk_size):
+        chunked_inputs.append(input_string[i : i + chunk_size])
+    return chunked_inputs
+
+
+def get_review(
+    model: str,
+    diff: str,
+    extra_prompt: str,
+    temperature: float,
+    max_tokens: int,
+    top_p: float,
+    frequency_penalty: float,
+    presence_penalty: float,
+    prompt_chunk_size: int,
+):
+    """Get a review"""
+    # Chunk the prompt
+    review_prompt = get_review_prompt(extra_prompt=extra_prompt)
+    chunked_diff_list = chunk_string(input_string=diff, chunk_size=prompt_chunk_size)
+    generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 0,
+        "max_output_tokens": 8192,
+    }
+    genai_model = genai.GenerativeModel(
+        model_name=model, generation_config=generation_config
+    )
+    # Get summary by chunk
+    chunked_reviews = []
+    for chunked_diff in chunked_diff_list:
+        convo = genai_model.start_chat(
+            history=[
+                {"role": "user", "parts": [review_prompt]},
+                {"role": "model", "parts": ["Ok"]},
+            ]
+        )
+        convo.send_message(chunked_diff)
+        review_result = convo.last.text
+        chunked_reviews.append(review_result)
+    # If the chunked reviews are only one, return it
+
+    if len(chunked_reviews) == 1:
+        return chunked_reviews, chunked_reviews[0]
+
+    if len(chunked_reviews) == 0:
+        summarize_prompt = (
+            "Say that you didn't find any relevant changes to comment on any file"
+        )
+    else:
+        summarize_prompt = get_summarize_prompt()
+
+    chunked_reviews_join = "\n".join(chunked_reviews)
+    convo = genai_model.start_chat(history=[])
+    convo.send_message(summarize_prompt + "\n\n" + chunked_reviews_join)
+    summarized_review = convo.last.text
+    logger.debug(f"Response AI: {summarized_review}")
+    return chunked_reviews, summarized_review
+
+
+def format_review_comment(summarized_review: str, chunked_reviews: List[str]) -> str:
+    """Format reviews"""
+    if len(chunked_reviews) == 1:
+        return summarized_review
+    unioned_reviews = "\n".join(chunked_reviews)
+    review = f"""<details>
+    <summary>{summarized_review}</summary>
+    {unioned_reviews}
+    </details>
+    """
+    return review
+
+
+def get_review_summary(
+    diff: str,
+    api_key: str,
+    diff_chunk_size=3500,
+    model="gemini-1.5-flash",
+    extra_prompt="",
+    temperature=0.1,
+    max_tokens=512,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    log_level="INFO",
+):
+    # Set log level
+    logger.level(log_level)
+    
+    # Set the Gemini API key
+    genai.configure(api_key=api_key)
+
+    # Request a code review
+    chunked_reviews, summarized_review = get_review(
+        diff=diff,
+        extra_prompt=extra_prompt,
+        model=model,
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
+        frequency_penalty=frequency_penalty,
+        presence_penalty=presence_penalty,
+        prompt_chunk_size=diff_chunk_size,
+    )
+
+    # Format reviews
+    review_comment = format_review_comment(
+        summarized_review=summarized_review, chunked_reviews=chunked_reviews
+    )
+    return review_comment
+    # # Create a comment to a pull request
+    # create_a_comment_to_pull_request(
+    #     github_token=os.getenv("GITHUB_TOKEN"),
+    #     github_repository=os.getenv("GITHUB_REPOSITORY"),
+    #     pull_request_number=int(os.getenv("GITHUB_PULL_REQUEST_NUMBER")),
+    #     git_commit_hash=os.getenv("GIT_COMMIT_HASH"),
+    #     body=review_comment
+    # )
